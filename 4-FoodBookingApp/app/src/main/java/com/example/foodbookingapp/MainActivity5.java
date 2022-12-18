bc2b1be05031876ca835d62579dfe9d8d6071271
Anjali2201 <anjalikushwaha031@gmail.com>
2022-12-18 12:08:22 +0530

    .

--- 4-FoodBookingApp/app/src/main/java/com/example/foodbookingapp/MainActivity5.java
+++ 4-FoodBookingApp/app/src/main/java/com/example/foodbookingapp/MainActivity5.java
@@ -1,5 +1,6 @@
 package com.example.foodbookingapp;
 
+import android.app.AlertDialog;
 import android.app.Notification;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
@@ -27,6 +28,10 @@ import java.util.ArrayList;
 
 public class MainActivity5 extends AppCompatActivity {
 
+    AlertDialog dialog;
+    AlertDialog.Builder builder;
+    String[] options = { "Home Delivery-30 mins", "Store Pickup-20 mins" };
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -41,16 +46,17 @@ public class MainActivity5 extends AppCompatActivity {
         ListView listView2 = (ListView) findViewById(R.id.list2);
         TextView total = (TextView) findViewById(R.id.total);
         Button btn = (Button) findViewById(R.id.button4);
-        registerForContextMenu(btn);
-
-
-
-
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dialog = builder.create();
+                dialog.show();
+            }
+        });
+        // registerForContextMenu(btn);
 
         final NotificationManager mgr = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
         createChannel(mgr);
-
-
 
         clear.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -76,6 +82,43 @@ public class MainActivity5 extends AppCompatActivity {
         }
         total.setText("Total: " + totalCost);
 
+        builder = new AlertDialog.Builder(this);
+        builder.setTitle("Choose an option");
+        builder.setItems(options, (dialog, which) -> {
+            Toast.makeText(this, "Selected Item: " + options[which], Toast.LENGTH_SHORT).show();
+            if (options[which] == "Home Delivery-30 mins") {
+                Intent i = new Intent(MainActivity5.this, MainActivity6.class);
+                i.putExtra("Message", "Your order has been placed Successfully!");
+                PendingIntent pi = PendingIntent.getActivity(MainActivity5.this, 0, i, 0);
+                NotificationCompat.Builder nBuilder = new NotificationCompat.Builder(MainActivity5.this,
+                        "mychannels.Apps");
+                Notification n = nBuilder
+                        .setSmallIcon(R.drawable.icon)
+                        .setContentTitle("Ordered Successfully!")
+                        .setWhen(System.currentTimeMillis())
+                        .setContentIntent(pi)
+                        .build();
+                mgr.notify(0, n);
+                Intent i2 = new Intent(MainActivity5.this, MainActivity.class);
+                startActivity(i2);
+            } else if (options[which] == "Store Pickup-20 mins") {
+                Intent i = new Intent(MainActivity5.this, MainActivity6.class);
+                i.putExtra("Message", "Your order has been placed Successfully!");
+                PendingIntent pi = PendingIntent.getActivity(MainActivity5.this, 0, i, 0);
+                NotificationCompat.Builder nBuilder = new NotificationCompat.Builder(MainActivity5.this,
+                        "mychannels.Apps");
+                Notification n = nBuilder
+                        .setSmallIcon(R.drawable.icon)
+                        .setContentTitle("Ordered Successfully!")
+                        .setWhen(System.currentTimeMillis())
+                        .setContentIntent(pi)
+                        .build();
+                mgr.notify(0, n);
+                Intent i2 = new Intent(MainActivity5.this, MainActivity.class);
+                startActivity(i2);
+            }
+        });
+
     }
 
     protected void createChannel(NotificationManager mgr) {
@@ -91,52 +134,57 @@ public class MainActivity5 extends AppCompatActivity {
         }
     }
 
-    @Override
-    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
-        super.onCreateContextMenu(menu, v, menuInfo);
-        MenuInflater inflater = getMenuInflater();
-        menu.setHeaderTitle("Choose an option");
-        menu.add(0, v.getId(), 0, "Home Delivery-30 mins");
-        menu.add(0, v.getId(), 0, "Store Pickup-20 mins");
+    // @Override
+    // public void onCreateContextMenu(ContextMenu menu, View v,
+    // ContextMenu.ContextMenuInfo menuInfo) {
+    // super.onCreateContextMenu(menu, v, menuInfo);
+    // MenuInflater inflater = getMenuInflater();
+    // menu.setHeaderTitle("Choose an option");
+    // menu.add(0, v.getId(), 0, "Home Delivery-30 mins");
+    // menu.add(0, v.getId(), 0, "Store Pickup-20 mins");
+    //
+    // }
 
-    }
-
-    @Override
-    public boolean onContextItemSelected(MenuItem item) {
-        Toast.makeText(this, "Selected Item: " + item.getTitle(), Toast.LENGTH_SHORT).show();
-        final NotificationManager mgr = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
-        createChannel(mgr);
-        if (item.getTitle() == "Home Delivery-30 mins") {
-            Intent i = new Intent(MainActivity5.this, MainActivity6.class);
-            i.putExtra("Message", "Your order has been placed Successfully!");
-            PendingIntent pi = PendingIntent.getActivity(MainActivity5.this, 0, i, 0);
-            NotificationCompat.Builder nBuilder = new NotificationCompat.Builder(MainActivity5.this,
-                    "mychannels.Apps");
-            Notification n = nBuilder
-                    .setSmallIcon(R.drawable.icon)
-                    .setContentTitle("Ordered Successfully!")
-                    .setWhen(System.currentTimeMillis())
-                    .setContentIntent(pi)
-                    .build();
-            mgr.notify(11, n);
-
-        } else if (item.getTitle() == "Store Pickup-20 mins") {
-            Intent i = new Intent(MainActivity5.this, MainActivity7.class);
-            i.putExtra("Message", "Your order has been placed Successfully!");
-            PendingIntent pi = PendingIntent.getActivity(MainActivity5.this, 0, i, 0);
-
-            // CREATION OF NOTIFICATION
-            NotificationCompat.Builder nBuilder = new NotificationCompat.Builder(MainActivity5.this,
-                    "mychannels.Apps");
-            Notification n = nBuilder
-                    .setSmallIcon(R.drawable.icon)
-                    .setContentTitle("Ordered Successfully!")
-                    .setWhen(System.currentTimeMillis())
-                    .setContentIntent(pi)
-                    .build();
-            mgr.notify(11, n);
-        }
-        return true;
-    }
+    // @Override
+    // public boolean onContextItemSelected(MenuItem item) {
+    // Toast.makeText(this, "Selected Item: " + item.getTitle(),
+    // Toast.LENGTH_SHORT).show();
+    // final NotificationManager mgr = (NotificationManager)
+    // getSystemService(Context.NOTIFICATION_SERVICE);
+    // createChannel(mgr);
+    // if (item.getTitle() == "Home Delivery-30 mins") {
+    // Intent i = new Intent(MainActivity5.this, MainActivity6.class);
+    // i.putExtra("Message", "Your order has been placed Successfully!");
+    // PendingIntent pi = PendingIntent.getActivity(MainActivity5.this, 0, i, 0);
+    // NotificationCompat.Builder nBuilder = new
+    // NotificationCompat.Builder(MainActivity5.this,
+    // "mychannels.Apps");
+    // Notification n = nBuilder
+    // .setSmallIcon(R.drawable.icon)
+    // .setContentTitle("Ordered Successfully!")
+    // .setWhen(System.currentTimeMillis())
+    // .setContentIntent(pi)
+    // .build();
+    // mgr.notify(11, n);
+    //
+    // } else if (item.getTitle() == "Store Pickup-20 mins") {
+    // Intent i = new Intent(MainActivity5.this, MainActivity7.class);
+    // i.putExtra("Message", "Your order has been placed Successfully!");
+    // PendingIntent pi = PendingIntent.getActivity(MainActivity5.this, 0, i, 0);
+    //
+    // // CREATION OF NOTIFICATION
+    // NotificationCompat.Builder nBuilder = new
+    // NotificationCompat.Builder(MainActivity5.this,
+    // "mychannels.Apps");
+    // Notification n = nBuilder
+    // .setSmallIcon(R.drawable.icon)
+    // .setContentTitle("Ordered Successfully!")
+    // .setWhen(System.currentTimeMillis())
+    // .setContentIntent(pi)
+    // .build();
+    // mgr.notify(11, n);
+    // }
+    // return true;
+    // }
 
 }
